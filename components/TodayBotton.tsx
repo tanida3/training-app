baa39f52e927a3246ad91d8d98cb29db3acf0169
Hikaru <hikaru.012723@gmail.com>
2025-10-27 11:35:33 +0900

    修正完了

--- components/TodayBotton.tsx
+++ components/TodayBotton.tsx
@@ -1,7 +1,4 @@
 'use client';
-import Link from "next/link";
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="flex gap-6 p-6 bg-gray-100 min-h-screen">
